a74611270cf5a45fa780ada5008d69b74c5c097a
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2021-02-14 08:23:17 +0000

    add options
    画面に同時に表示するページ数を指定可能にした
    同時にヘルプを表示するオプションも追加した

--- pdfm.sh
+++ pdfm.sh
@@ -1,7 +1,43 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-	echo "Usage: `basename $0` pdf-file"
+MYNAME=`basename $0`
+IMAGEWIDTH="43%"
+
+while [ "$1" ]; do
+	case "$1" in
+	--)
+		shift
+		break
+		;;
+	-1)
+		IMAGEWIDTH="80%"
+		;;
+	-2)
+		IMAGEWIDTH="43%"
+		;;
+	-h|--help)
+		echo "Usage: $MYNAME [options] <PDF-File>"
+		echo ""
+		echo "option:"
+		echo "    -1           Display two pages on the screen."
+		echo "    -2           Dispaey one page on the screen."
+		echo "    -h, --help   Display this help message."
+		exit 0
+		;;
+	-*)
+		echo "ERROR: Unknown Option -- $1" >&2
+		echo "Try $MYNAME -h for more details." >&2
+		exit 1
+		;;
+	*)
+		break
+	esac
+	shift
+done
+
+if [ -z "$1" ]; then
+	echo "ERROR: No File specified." >&2
+	echo "Try $MYNAME -h for more details." >&2
 	exit 1
 fi
 
@@ -33,7 +69,7 @@ EOF
 
 find $TEMPDIR/*.png | sort | while read line
 do
-	echo "<IMG SRC=\"$line\" WIDTH=\"43%\">" >> $TEMPHTML
+	echo "<IMG SRC=\"$line\" WIDTH=\"$IMAGEWIDTH\">" >> $TEMPHTML
 done
 
 cat << EOF >> $TEMPHTML
